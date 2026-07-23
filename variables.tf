6e1ff4a949ee59a42eecbdf66f2b2b8bb4c1f88b
hrabadia05 <hrabadia0905@gmail.com>
2026-07-22 18:38:12 -0700

    Refactor infrastructure variables in variables.tf
    Updated variable descriptions and added new variables for environment, DynamoDB table name, S3 prefix, and pre-signed URL expiration settings.

--- variables.tf
+++ variables.tf
@@ -1,11 +1,45 @@
+# ==========================================
+# GAZEEBO INFRASTRUCTURE VARIABLES
+# ==========================================
+
 variable "aws_region" {
-  description = "The AWS region to deploy resources into"
+  description = "The AWS region where all infrastructure resources will be deployed."
   type        = string
   default     = "us-west-1"
 }
 
-variable "project_name" {
-  description = "Name prefix for all resources"
+variable "app_name" {
+  description = "Application name prefix used for naming resources."
   type        = string
-  default     = "gazeebo-portal"
+  default     = "gazeebo"
+}
+
+variable "environment" {
+  description = "Deployment environment (e.g., dev, staging, prod)."
+  type        = string
+  default     = "prod"
+}
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table used for metadata storage and state locking."
+  type        = string
+  default     = "GazeeboPortalData"
+}
+
+variable "s3_prefix" {
+  description = "Prefix folder within the private S3 bucket where photo assets are stored."
+  type        = string
+  default     = "photos/"
+}
+
+variable "presigned_url_expiration_read" {
+  description = "Expiration time in seconds for GET pre-signed view URLs."
+  type        = number
+  default     = 3600 # 1 hour
+}
+
+variable "presigned_url_expiration_upload" {
+  description = "Expiration time in seconds for POST pre-signed upload URLs."
+  type        = number
+  default     = 300 # 5 minutes
 }
